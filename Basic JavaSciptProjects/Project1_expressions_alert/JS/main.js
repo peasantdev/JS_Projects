dd9ee31fef562fd89840f59ff79443adf111daf2
peasantdev <116978007+peasantdev@users.noreply.github.com>
2022-12-15 17:01:12 -0800

    Add files via upload

---
+++ Basic JavaSciptProjects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,9 @@
+var Sent1 = "This is the beginning of the string"
+var Sent2 = " and this is the end of the string"
+document.write(Sent1 + Sent2); //This is concatenating the var Sent1 & Sent2 together, then printing them both on the screen.
+
+window.alert("There is hardly anything to see here!")
+
+var X = 7
+var Y = 10
+document.write(X * Y);
