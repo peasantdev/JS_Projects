7cdf4782c7ac017f0549ea0a967e606c25a3532f
peasantdev <peasantwebdev@outlook.com>
2022-12-16 16:01:30 -0800

    New files

---
+++ Basic JavaSciptProjects/Project2_functions/JS/main.js
@@ -0,0 +1,6 @@
+function My_First_Function() {
+    var str = "Click this button to do nothing!";
+    str += " I was serious the button does nothing!";
+
+    document.getElementById("Button_Text").innerHTML = str;
+}
