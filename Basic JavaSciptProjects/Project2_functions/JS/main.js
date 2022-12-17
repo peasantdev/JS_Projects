424385167f6f39a60ca8fa58dd97973847711167
peasantdev <peasantwebdev@outlook.com>
2022-12-16 16:05:41 -0800

    Commenting code

--- Basic JavaSciptProjects/Project2_functions/JS/main.js
+++ Basic JavaSciptProjects/Project2_functions/JS/main.js
@@ -1,6 +1,6 @@
-function My_First_Function() {
-    var str = "Click this button to do nothing!";
-    str += " I was serious the button does nothing!";
+function My_First_Function() {                    //Defining the function and giving it a name
+    var str = "Click this button to do nothing!"; //Defining a variable as a string
+    str += " I was serious the button does nothing!"; //Concatanating a string
 
-    document.getElementById("Button_Text").innerHTML = str;
+    document.getElementById("Button_Text").innerHTML = str; //Placing the value of the variable with the "Button_Text" ID
 }
